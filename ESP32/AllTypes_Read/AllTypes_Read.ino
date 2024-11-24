01086f9c64578e4c693eaa15294dd99e065baf11
Petr Šatka <sat@anet.eu>
2024-11-24 14:46:37 +0100

    Comments

--- ESP32/AllTypes_Read/AllTypes_Read.ino
+++ ESP32/AllTypes_Read/AllTypes_Read.ino
@@ -3,12 +3,15 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-#define SERVICE_UUID        "00000001-74ee-43ce-86b2-0dde20dcefd6b"
+// UUID for the BLE service
+#define SERVICE_UUID        "00000001-74ee-43ce-86b2-0dde20dcefd6"
+// UUID for the BLE characteristic
 #define CHARACTERISTIC_UUID "10000001-74ee-43ce-86b2-0dde20dcefd6"
-//Default UUID mask for Minglee app is ####face-####-####-####-############
-//Only face is FACE is significant value for descriptor edentification by Minglee
+// Default UUID mask for the Minglee app is ####face-####-####-####-############
+// The segment "face" (case-insensitive) is used by Minglee to identify descriptors
 #define CUSTOM_DESCRIPTOR_UUID  "2000face-74ee-43ce-86b2-0dde20dcefd6"
 
+// Custom server callback class to handle connection events
 class ServerCallbacks : public BLEServerCallbacks {
     void onConnect(BLEServer* pServer) {
         Serial.println("Device connected.");
@@ -16,7 +19,7 @@ class ServerCallbacks : public BLEServerCallbacks {
 
     void onDisconnect(BLEServer* pServer) {
         Serial.println("Device disconnected. Restarting advertising...");
-        // Restart advertising after disconnect
+        // Restart advertising when a device disconnects
         BLEDevice::startAdvertising();
     }
 };
@@ -24,55 +27,63 @@ class ServerCallbacks : public BLEServerCallbacks {
 void setup() {
     Serial.begin(115200);
 
-    //BLE Initialization
-    BLEDevice::init("Minglee device"); //Device name
+    // Initialize BLE device with a name
+    BLEDevice::init("Minglee device");
 
+    // Configure BLE security settings
     BLESecurity *pSecurity = new BLESecurity();
-    //Set ESP_LE_AUTH_REQ_SC_MITM_BOND for bonding
+    // No bonding (ESP_LE_AUTH_NO_BOND)
+    // Set ESP_LE_AUTH_REQ_SC_MITM_BOND to enable bonding
     pSecurity->setAuthenticationMode(ESP_LE_AUTH_NO_BOND);
     
-    //Uncomment for PIN bonding
-    //pSecurity->setCapability(ESP_IO_CAP_IO);
-    //pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
-    //pSecurity->setStaticPIN(123456);  
+    // Uncomment the following lines to enable PIN-based bonding
+    // pSecurity->setCapability(ESP_IO_CAP_IO);
+    // pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
+    // pSecurity->setStaticPIN(123456);
 
-    //BLE server
+    // Create a BLE server and set its callback class
     BLEServer *pServer = BLEDevice::createServer();
     pServer->setCallbacks(new ServerCallbacks());
 
-    //BLE service
+    // Create a BLE service with a predefined UUID
     BLEService *pService = pServer->createService(SERVICE_UUID);
 
-    //BLE characteristic
+    // Create a BLE characteristic with read and notify properties
     BLECharacteristic *pCharacteristic = pService->createCharacteristic(
                                          CHARACTERISTIC_UUID,
                                          BLECharacteristic::PROPERTY_READ |
-                                         BLECharacteristic::PROPERTY_WRITE |
                                          BLECharacteristic::PROPERTY_NOTIFY
                                        );
-    
-    //Uncomment for characteristic encryption
-    //pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
 
-    //Custom descriptor form Minglee app
+    // Uncomment the following line to enable encrypted access to the characteristic
+    // pCharacteristic->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED | ESP_GATT_PERM_WRITE_ENCRYPTED);
+
+    // Add a custom descriptor used by the Minglee app
     BLEDescriptor *customDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID);
-    customDescriptor->setValue("My descriptor"); // Nastavení hodnoty, např. jednotky měření
+    //Set control configuration
+    customDescriptor->setValue(
+R"(
+{
+  "type":"title"
+}
+)");
     pCharacteristic->addDescriptor(customDescriptor);
 
-    //Charakteristic value
-    pCharacteristic->setValue("Hello, BLE!");
+    // Set an initial value for the characteristic
+    pCharacteristic->setValue("This Is Title");
 
-    //Start service
+    // Start the BLE service
     pService->start();
 
-    //Start adveritising
+    // Start BLE advertising
     BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
-    pAdvertising->addServiceUUID(SERVICE_UUID);
-    pAdvertising->setScanResponse(true);
+    pAdvertising->addServiceUUID(SERVICE_UUID); // Advertise the service UUID
+    pAdvertising->setScanResponse(true);       // Enable scan response
     BLEDevice::startAdvertising();
 
     Serial.println("BLE server is running and advertising...");
 }
 
 void loop() {
+    // Empty loop since BLE server runs in the background
 }
