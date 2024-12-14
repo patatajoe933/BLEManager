4f83f75a3aba26236d66700577b8b590fa49d0bb
Petr Šatka <sat@anet.eu>
2024-12-14 15:52:22 +0100

    Signed integers

--- ESP32/SignedIntegers/SignedIntegers.ino
+++ ESP32/SignedIntegers/SignedIntegers.ino
@@ -4,13 +4,16 @@
 #include <BLE2902.h>
 
 // UUID for the BLE service
-#define SERVICE_UUID                      "00000021-74ee-43ce-86b2-0dde20dcefd6"
+#define SERVICE_UUID "00000020-74ee-43ce-86b2-0dde20dcefd6"
 // UUIDs for BLE characteristics
-#define CHARACTERISTIC_SERVICE_NAME_UUID  "10000020-74ee-43ce-86b2-0dde20dcefd6"
-#define CHARACTERISTIC_SINT8_UUID         "10000021-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SERVICE_NAME_UUID "10000020-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT8_UUID "10000021-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT16_UUID "10000022-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT32_UUID "10000023-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT64_UUID "10000024-74ee-43ce-86b2-0dde20dcefd6"
 // Default UUID mask for the Minglee app is ####face-####-####-####-############
 // The segment "face" (case-insensitive) is used by Minglee to identify descriptors
-#define CUSTOM_DESCRIPTOR_UUID            "2000face-74ee-43ce-86b2-0dde20dcefd6"
+#define CUSTOM_DESCRIPTOR_UUID "2000face-74ee-43ce-86b2-0dde20dcefd6"
 
 // Custom server callback class to handle connection events
 class ServerCallbacks : public BLEServerCallbacks {
@@ -36,27 +39,17 @@ class IntegerCharacteristicCallbacks : public BLECharacteristicCallbacks {
       int64_t intValue = 0;
 
       switch (dataLength) {
-        case 1: // 8-bit integer
+        case 1:  // 8-bit integer
           intValue = static_cast<int8_t>(data[0]);
           break;
-        case 2: // 16-bit integer
+        case 2:  // 16-bit integer
           intValue = static_cast<int16_t>(data[0] | (data[1] << 8));
           break;
-        case 4: // 32-bit integer
-          intValue = static_cast<int32_t>(data[0] |
-                                          (data[1] << 8) |
-                                          (data[2] << 16) |
-                                          (data[3] << 24));
+        case 4:  // 32-bit integer
+          intValue = static_cast<int32_t>(data[0] | (data[1] << 8) | (data[2] << 16) | (data[3] << 24));
           break;
-        case 8: // 64-bit integer
-          intValue = static_cast<int64_t>(data[0] |
-                                          (uint64_t(data[1]) << 8) |
-                                          (uint64_t(data[2]) << 16) |
-                                          (uint64_t(data[3]) << 24) |
-                                          (uint64_t(data[4]) << 32) |
-                                          (uint64_t(data[5]) << 40) |
-                                          (uint64_t(data[6]) << 48) |
-                                          (uint64_t(data[7]) << 56));
+        case 8:  // 64-bit integer
+          intValue = static_cast<int64_t>(data[0] | (uint64_t(data[1]) << 8) | (uint64_t(data[2]) << 16) | (uint64_t(data[3]) << 24) | (uint64_t(data[4]) << 32) | (uint64_t(data[5]) << 40) | (uint64_t(data[6]) << 48) | (uint64_t(data[7]) << 56));
           break;
         default:
           Serial.println("Invalid data length!");
@@ -76,17 +69,13 @@ void setup() {
   // Initialize BLE device with a name
   BLEDevice::init("Minglee device");
 
-  //BLESecurity *pSecurity = new BLESecurity();
-  //pSecurity->setAuthenticationMode(ESP_LE_AUTH_BOND);
-  //pSecurity->setInitEncryptionKey(ESP_BLE_ENC_KEY_MASK | ESP_BLE_ID_KEY_MASK);
-
   // Create a BLE server and set its callback class
   BLEServer *pServer = BLEDevice::createServer();
   pServer->setCallbacks(new ServerCallbacks());
 
   // Create a BLE service with a predefined UUID
   // Service handles by default 15 handles. Each BLE Characteristic takes 2 handles and each BLE Descriptor takes 1 handle.
-  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 15); //15 Handles
+  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 20);  //20 Handles
   // If you add or remove characteristics, it may be necessary to forget the device (if paired)
   // in the Bluetooth settings and re-pair it on Android for changes to take effect.
   // Alternatively you can try Clear GATT cahce from device menu un MingleApp
@@ -100,12 +89,12 @@ void setup() {
   BLECharacteristic *pCharacteristicServiceName = pService->createCharacteristic(
     CHARACTERISTIC_SERVICE_NAME_UUID,
     BLECharacteristic::PROPERTY_READ);
-  //pCharacteristicServiceName->setAccessPermissions(ESP_GATT_PERM_READ_ENCRYPTED); //WORKS ONLY WITH BONDING ENABLED -- Pokud se použije bez bondingu, způsobí nemožnost opakovaného připojení.
 
   // Add a custom descriptor used by the Minglee app
   // Only one descriptor matching the mask is supported per characteristic.
   // If multiple descriptors match, one may be selected randomly.
-  BLEDescriptor *serviceNameDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID);
+  //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *serviceNameDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
   // Set control configuration
   // JSON format. Keys are case-sensitive.
   serviceNameDescriptor->setValue(
@@ -125,17 +114,58 @@ void setup() {
   // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
   BLECharacteristic *pCharacteristicSInt8 = pService->createCharacteristic(
     CHARACTERISTIC_SINT8_UUID,
-    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
-  );
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
   pCharacteristicSInt8->setCallbacks(new IntegerCharacteristicCallbacks());
-  BLEDescriptor *sInt8Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID);
+  //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *sInt8Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
   sInt8Descriptor->setValue(
-    R"({"type":"sint8", "order":1, "disabled":false, label:"Signed Byte"})");
+    R"({"type":"sint8", "order":1, "disabled":false, "label":"Signed Byte", "minInt":-70, "maxInt":70})");
 
   pCharacteristicSInt8->addDescriptor(sInt8Descriptor);
-  uint8_t b = -69; 
-  pCharacteristicSInt8->setValue(&b, 1);
+  uint8_t sint8 = -69;
+  pCharacteristicSInt8->setValue(&sint8, sizeof(uint8_t));
 
+  // sint16: Editable control for signed int16 characteristics
+  // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
+  BLECharacteristic *pCharacteristicSInt16 = pService->createCharacteristic(
+    CHARACTERISTIC_SINT16_UUID,  // Correct UUID for sint16
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristicSInt16->setCallbacks(new IntegerCharacteristicCallbacks());
+   //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *sInt16Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  sInt16Descriptor->setValue(
+    R"({"type":"sint16", "order":1, "disabled":false, "label":"Signed Int16", "minInt":-7070, "maxInt":7070})");
+  pCharacteristicSInt16->addDescriptor(sInt16Descriptor);
+  int16_t sint16 = -6969;
+  pCharacteristicSInt16->setValue((uint8_t *)&sint16, sizeof(int16_t));
+
+  // sint32: Editable control for signed int32 characteristics
+  // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
+  BLECharacteristic *pCharacteristicSInt32 = pService->createCharacteristic(
+    CHARACTERISTIC_SINT32_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristicSInt32->setCallbacks(new IntegerCharacteristicCallbacks());
+   //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *sInt32Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  sInt32Descriptor->setValue(
+    R"({"type":"sint32", "order":2, "disabled":false, "label":"Signed Int32", "minInt":-707070, "maxInt":707070})");
+  pCharacteristicSInt32->addDescriptor(sInt32Descriptor);
+  int32_t sint32 = -696969;
+  pCharacteristicSInt32->setValue((uint8_t *)&sint32, sizeof(int32_t));
+
+  // sint64: Editable control for signed int64 characteristics
+  // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
+  BLECharacteristic *pCharacteristicSInt64 = pService->createCharacteristic(
+    CHARACTERISTIC_SINT64_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
+  pCharacteristicSInt64->setCallbacks(new IntegerCharacteristicCallbacks());
+   //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *sInt64Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  sInt64Descriptor->setValue(
+    R"({"type":"sint64", "order":3, "disabled":false, "label":"Signed Int64", "minInt":-7070707070, "maxInt":7070707070})");
+  pCharacteristicSInt64->addDescriptor(sInt64Descriptor);
+  int64_t sint64 = -6969696969;
+  pCharacteristicSInt64->setValue((uint8_t *)&sint64, sizeof(int64_t));
 
   // Start the BLE service
   pService->start();
@@ -150,5 +180,4 @@ void setup() {
 }
 
 void loop() {
-
 }
