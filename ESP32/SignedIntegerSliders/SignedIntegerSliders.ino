0f1a7b0220f98e46ed0f7571925134c439d0f440
Petr Šatka <sat@anet.eu>
2024-12-17 21:04:51 +0100

    Signed integer sliders

--- ESP32/SignedIntegerSliders/SignedIntegerSliders.ino
+++ ESP32/SignedIntegerSliders/SignedIntegerSliders.ino
@@ -3,41 +3,15 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
-/*
-ESP32 transmits data in Little-Endian byte order. To emulate Big-Endian byte order, we must reverse the byte order.
-*/
-
 // UUID for the BLE service
-#define SERVICE_UUID "000000C0-74ee-43ce-86b2-0dde20dcefd6"
+#define SERVICE_UUID "000000D0-74ee-43ce-86b2-0dde20dcefd6"
 // UUIDs for BLE characteristics
-#define CHARACTERISTIC_SERVICE_NAME_UUID "100000C0-74ee-43ce-86b2-0dde20dcefd6"
-#define CHARACTERISTIC_HALF_UUID "100000C1-74ee-43ce-86b2-0dde20dcefd6"
-#define CHARACTERISTIC_FLOAT_UUID "100000C2-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SERVICE_NAME_UUID "100000D0-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT8_UUID "100000D1-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_SINT16_UUID "100000D2-74ee-43ce-86b2-0dde20dcefd6"
 // Default UUID mask for the Minglee app is ####face-####-####-####-############
 // The segment "face" (case-insensitive) is used by Minglee to identify descriptors
 #define CUSTOM_DESCRIPTOR_UUID "2000face-74ee-43ce-86b2-0dde20dcefd6"
-
-float byteswap32(float x) {
-  uint32_t temp;
-  memcpy(&temp, &x, sizeof(temp));
-
-  temp = ((temp & 0xFF) << 24) | ((temp >> 8 & 0xFF) << 16) | ((temp >> 16 & 0xFF) << 8) | ((temp >> 24 & 0xFF));
-
-  float result;
-  memcpy(&result, &temp, sizeof(result));
-  return result;
-}
-
-double byteswap64(double x) {
-  uint64_t temp;
-  memcpy(&temp, &x, sizeof(temp));
-
-  temp = ((temp & 0xFF) << 56) | ((temp >> 8 & 0xFF) << 48) | ((temp >> 16 & 0xFF) << 40) | ((temp >> 24 & 0xFF) << 32) | ((temp >> 32 & 0xFF) << 24) | ((temp >> 40 & 0xFF) << 16) | ((temp >> 48 & 0xFF) << 8) | ((temp >> 56 & 0xFF));
-
-  double result;
-  memcpy(&result, &temp, sizeof(result));
-  return result;
-}
 
 // Custom server callback class to handle connection events
 class ServerCallbacks : public BLEServerCallbacks {
@@ -52,8 +26,7 @@ class ServerCallbacks : public BLEServerCallbacks {
   }
 };
 
-//Big Endian
-class FloatCharacteristicCallbacks : public BLECharacteristicCallbacks {
+class IntegerCharacteristicCallbacks : public BLECharacteristicCallbacks {
   void onWrite(BLECharacteristic *pCharacteristic) override {
     const uint8_t *data = pCharacteristic->getData();
     size_t dataLength = pCharacteristic->getLength();
@@ -61,43 +34,40 @@ class FloatCharacteristicCallbacks : public BLECharacteristicCallbacks {
     Serial.print("Received value: ");
 
     if (dataLength > 0) {
-      if (dataLength == 2) {  // 16-bit halfFloat
-        uint16_t halfValue = (data[0] << 8) | data[1];
-        float floatValue = halfToFloat(halfValue);  //halfFloat -> float
-        Serial.println(floatValue);
-      } else if (dataLength == 4) {  // 32-bit Float
-        uint32_t intValue = (data[0] << 24) | (data[1] << 16) | (data[2] << 8) | data[3];
-        float floatValue;
-        memcpy(&floatValue, &intValue, sizeof(float));
-        Serial.println(floatValue);
-      } else if (dataLength == 8) {  // 64-bit Double
-        uint64_t intValue = (static_cast<uint64_t>(data[0]) << 56) | (static_cast<uint64_t>(data[1]) << 48) | (static_cast<uint64_t>(data[2]) << 40) | (static_cast<uint64_t>(data[3]) << 32) | (static_cast<uint64_t>(data[4]) << 24) | (static_cast<uint64_t>(data[5]) << 16) | (static_cast<uint64_t>(data[6]) << 8) | static_cast<uint64_t>(data[7]);
-        double doubleValue;
-        memcpy(&doubleValue, &intValue, sizeof(double));
-        Serial.println(doubleValue);
-      } else {
-        Serial.println("Invalid data length for floating-point value!");
+      switch (dataLength) {
+        case sizeof(int8_t):  // 8-bit integer
+          {
+            int8_t intValue;
+            memcpy(&intValue, data, sizeof(int8_t));
+            Serial.println(intValue);
+          }
+          break;
+        case sizeof(int16_t):  // 16-bit integer
+          {
+            int16_t intValue;
+            memcpy(&intValue, data, sizeof(int16_t));
+            Serial.println(intValue);
+          }
+          break;
+        case sizeof(int32_t):  // 32-bit integer
+          {
+            int32_t intValue;
+            memcpy(&intValue, data, sizeof(int32_t));
+            Serial.println(intValue);
+          }
+          break;
+        case sizeof(int64_t):  // 64-bit integer
+          {
+            int64_t intValue;
+            memcpy(&intValue, data, sizeof(int64_t));
+            Serial.println(intValue);
+          }
+          break;
+        default:
+          Serial.println("Invalid data length!");
       }
     } else {
       Serial.println("Empty value received!");
-    }
-  }
-
-  float halfToFloat(uint16_t half) {
-    uint16_t sign = (half & 0x8000) >> 15;
-    uint16_t exponent = (half & 0x7C00) >> 10;
-    uint16_t mantissa = half & 0x03FF;
-
-    if (exponent == 0) {  // Subnormal number or zero
-      if (mantissa == 0) {
-        return sign ? -0.0f : 0.0f;
-      } else {
-        return (sign ? -1.0f : 1.0f) * (mantissa / 1024.0f) * pow(2, -14);
-      }
-    } else if (exponent == 0x1F) {  // Infinity or NaN
-      return mantissa ? NAN : (sign ? -INFINITY : INFINITY);
-    } else {  // Normal number
-      return (sign ? -1.0f : 1.0f) * (1.0f + (mantissa / 1024.0f)) * pow(2, exponent - 15);
     }
   }
 };
@@ -114,7 +84,7 @@ void setup() {
 
   // Create a BLE service with a predefined UUID
   // Service handles by default 15 handles. Each BLE Characteristic takes 2 handles and each BLE Descriptor takes 1 handle.
-  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 15);  //15 Handles
+  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 20);  //20 Handles
   // If you add or remove characteristics, it may be necessary to forget the device (if paired)
   // in the Bluetooth settings and re-pair it on Android for changes to take effect.
   // Alternatively you can try Clear GATT cahce from device menu un MingleApp
@@ -142,44 +112,41 @@ void setup() {
   pCharacteristicServiceName->addDescriptor(serviceNameDescriptor);
 
   // Set an initial value for the characteristic
-  pCharacteristicServiceName->setValue("Big Endian Float Sliders");
+  pCharacteristicServiceName->setValue("Signed Integer Sliders");
 
   //// CONTROLS ////
   // If you add or remove characteristics, it may be necessary to forget the device (if paired)
   // in the Bluetooth settings and re-pair it on Android for changes to take effect.
   // Alternatively you can try Clear GATT cahce from device menu un MingleApp
 
-  // HalfSlider: editable slider control for float16 characteristics
+  // sint8slider: editable slider control for signed byte characteristics
   // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
-  BLECharacteristic *pCharacteristicHalf = pService->createCharacteristic(
-    CHARACTERISTIC_HALF_UUID,
+  BLECharacteristic *pCharacteristicSInt8 = pService->createCharacteristic(
+    CHARACTERISTIC_SINT8_UUID,
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-  pCharacteristicHalf->setCallbacks(new FloatCharacteristicCallbacks());
+  pCharacteristicSInt8->setCallbacks(new IntegerCharacteristicCallbacks());
   //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
-  BLEDescriptor *halfDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
-  halfDescriptor->setValue(
-    R"({"type":"halfsliderbe", "order":1, "disabled":false, "label":"Float 16", "minFloat": 0, "maxFloat": 75, "stepFloat": 0.1})"); //Defaults: minFloat = 0, maxFloat = 100, stepFloat: 1
+  BLEDescriptor *sInt8Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  sInt8Descriptor->setValue(
+    R"({"type":"sint8slider", "order":1, "disabled":false, "label":"Signed Byte", "minInt":-50, "maxInt":50, "stepInt":1})"); //Defaults: minInt = 0, maxInt = 100, stepInt: 1
 
-  pCharacteristicHalf->addDescriptor(halfDescriptor);
-  uint16_t h = 0x4052; //50
-  pCharacteristicHalf->setValue((uint8_t *)&h, sizeof(uint16_t));
+  pCharacteristicSInt8->addDescriptor(sInt8Descriptor);
+  uint8_t sint8 = -10;
+  pCharacteristicSInt8->setValue(&sint8, sizeof(uint8_t));
 
-  // FloatSlider: editable slider control for float32 characteristics
+  // sint16slider: Editable slider control for signed int16 characteristics
   // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
-  BLECharacteristic *pCharacteristicFloat = pService->createCharacteristic(
-    CHARACTERISTIC_FLOAT_UUID,
+  BLECharacteristic *pCharacteristicSInt16 = pService->createCharacteristic(
+    CHARACTERISTIC_SINT16_UUID,  // Correct UUID for sint16
     BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE);
-
-  pCharacteristicFloat->setCallbacks(new FloatCharacteristicCallbacks());
+  pCharacteristicSInt16->setCallbacks(new IntegerCharacteristicCallbacks());
   //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
-  BLEDescriptor *floatDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
-  floatDescriptor->setValue(
-    R"({"type":"floatsliderbe", "order":2, "disabled":false, label:"Float 32", "minFloat": -50, "maxFloat": 50, "stepFloat": 1})"); //Defaults: minFloat = 0, maxFloat = 100, stepFloat: 1
-
-  pCharacteristicFloat->addDescriptor(floatDescriptor);
-
-  float f = byteswap32(0);
-  pCharacteristicFloat->setValue(f);
+  BLEDescriptor *sInt16Descriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  sInt16Descriptor->setValue(
+    R"({"type":"sint16slider", "order":2, "disabled":false, "label":"Signed Int16", "minInt":0, "maxInt":100, "stepInt":2})"); //Defaults: minInt = 0, maxInt = 100, stepInt: 1
+  pCharacteristicSInt16->addDescriptor(sInt16Descriptor);
+  int16_t sint16 = 50;
+  pCharacteristicSInt16->setValue((uint8_t *)&sint16, sizeof(int16_t));
 
   // Start the BLE service
   pService->start();
