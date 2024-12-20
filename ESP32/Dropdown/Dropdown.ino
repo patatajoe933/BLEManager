18dbe9ab91e81b2f86b5a61e8dc297bd5c1b0102
Petr Šatka <sat@anet.eu>
2024-12-20 18:55:49 +0100

    Dropdown

---
+++ ESP32/Dropdown/Dropdown.ino
@@ -0,0 +1,125 @@
+#include <BLEDevice.h>
+#include <BLEServer.h>
+#include <BLEUtils.h>
+#include <BLE2902.h>
+
+// UUID for the BLE service
+#define SERVICE_UUID                      "00000120-74ee-43ce-86b2-0dde20dcefd6"
+// UUIDs for BLE characteristics
+#define CHARACTERISTIC_SERVICE_NAME_UUID  "10000120-74ee-43ce-86b2-0dde20dcefd6"
+#define CHARACTERISTIC_DROPDOWN_UUID    "10000121-74ee-43ce-86b2-0dde20dcefd6"
+// Default UUID mask for the Minglee app is ####face-####-####-####-############
+// The segment "face" (case-insensitive) is used by Minglee to identify descriptors
+#define CUSTOM_DESCRIPTOR_UUID            "2000face-74ee-43ce-86b2-0dde20dcefd6"
+
+// Custom server callback class to handle connection events
+class ServerCallbacks : public BLEServerCallbacks {
+  void onConnect(BLEServer *pServer) {
+    Serial.println("Device connected.");
+  }
+
+  void onDisconnect(BLEServer *pServer) {
+    Serial.println("Device disconnected. Restarting advertising...");
+    // Restart advertising when a device disconnects
+    BLEDevice::startAdvertising();
+  }
+};
+
+class StringCharacteristicCallbacks : public BLECharacteristicCallbacks {
+  void onWrite(BLECharacteristic *pCharacteristic) override {
+    String value = pCharacteristic->getValue();
+    Serial.print("Received value: ");
+    if (!value.isEmpty()) {
+      Serial.println(value.c_str());
+    }
+  }
+};
+
+BLECharacteristic *testCharacteristic = NULL;
+void setup() {
+  Serial.begin(115200);
+
+  // Initialize BLE device with a name
+  BLEDevice::init("Minglee device");
+
+  // Create a BLE server and set its callback class
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new ServerCallbacks());
+
+  // Create a BLE service with a predefined UUID
+  // Service handles by default 15 handles. Each BLE Characteristic takes 2 handles and each BLE Descriptor takes 1 handle.
+  BLEService *pService = pServer->createService(BLEUUID(SERVICE_UUID), 20);  //20 handlers
+  // If you add or remove characteristics, it may be necessary to forget the device
+  // in the Bluetooth settings and re-pair it on Android for changes to take effect.
+
+  // Create a BLE characteristic for service name
+  // The value of this characteristic will be displayed as the service name.
+  // The "order" value determines the order in which the service appears in the Minglee app.
+  // Only one "serviceName" characteristic is supported per service.
+  // If a service contains multiple "serviceName" characteristics, one may be selected randomly.
+
+  BLECharacteristic *pCharacteristicServiceName = pService->createCharacteristic(
+    CHARACTERISTIC_SERVICE_NAME_UUID,
+    BLECharacteristic::PROPERTY_READ);
+
+  // Add a custom descriptor used by the Minglee app
+  // Only one descriptor matching the mask is supported per characteristic.
+  // If multiple descriptors match, one may be selected randomly.
+  //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *serviceNameDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  // Set control configuration
+  // JSON format. Keys are case-sensitive.
+  serviceNameDescriptor->setValue(
+    R"({"type":"serviceName", "order":1})");
+
+  pCharacteristicServiceName->addDescriptor(serviceNameDescriptor);
+
+  // Set an initial value for the characteristic
+  pCharacteristicServiceName->setValue("Dropdown");
+
+  //// CONTROLS ////
+  // If you add or remove characteristics, it may be necessary to forget the device
+  // in the Bluetooth settings and re-pair it on Android for changes to take effect.
+
+  // Dropdown: editable control for string characteristic
+  // Supports UTF-8 encoding
+  // If the characteristic is not writable, the "disabled" property is ignored, and the control remains disabled.
+  BLECharacteristic *pCharacteristicText = pService->createCharacteristic(
+    CHARACTERISTIC_DROPDOWN_UUID,
+    BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_WRITE
+    //| BLECharacteristic::PROPERTY_INDICATE
+  );
+  pCharacteristicText->setCallbacks(new StringCharacteristicCallbacks());
+  //! The default maximum length of a descriptor is 100 bytes. Setting a descriptor value that exceeds this limit will cause a crash during startup.
+  BLEDescriptor *textDescriptor = new BLEDescriptor(CUSTOM_DESCRIPTOR_UUID, 200);
+  textDescriptor->setValue(
+    R"({"type":"dropdown", "order":1, "disabled":false, label:"Dropdown Menu", "options":["Option1","Option2","Option3"]})");
+
+  pCharacteristicText->addDescriptor(textDescriptor);
+  pCharacteristicText->setValue("Option2");
+
+  ////NOTIFY TEST////
+  //testCharacteristic = pCharacteristicTitleView;
+  //testCharacteristic->addDescriptor(new BLE2902());
+  /////
+  // Start the BLE service
+  pService->start();
+
+  // Start BLE advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);  // Advertise the service UUID
+  pAdvertising->setScanResponse(true);         // Enable scan response
+  BLEDevice::startAdvertising();
+
+  Serial.println("BLE server is running and advertising...");
+}
+
+int counter = 0;
+void loop() {
+  // Empty loop since BLE server runs in the background
+  //NOTIFY TEST
+  /*delay(5000);
+  testCharacteristic->setValue(String(counter++));
+  testCharacteristic->indicate();*/
+  ///////////
+}
