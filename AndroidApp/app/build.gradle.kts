d5a25544574aaec4fb62f41ed25d5ce794b28f79
Petr Šatka <sat@anet.eu>
2025-03-01 21:52:27 +0100

    Release 19

--- AndroidApp/app/build.gradle.kts
+++ AndroidApp/app/build.gradle.kts
@@ -24,8 +24,8 @@ android {
         applicationId = "net.satka.bleManager"
         minSdk = 33
         targetSdk = 35
-        versionCode = 18
-        versionName = "18"
+        versionCode = 19
+        versionName = "19"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
